25305ee45b3109c71a96135fb902c4895c28c551
Marta SIWINSKA <msiwinska@splio.com>
2020-05-27 00:47:43 +0200

    funcionality

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
 // JavaScript Document
+var image = document.querySelector('.dog-image');
+
+function fetchDog() {
+	fetch('https://dog.ceo/api/breeds/image/random').then(res => { return res.json();})
+	.then(jsonRes => {
+		image.src = jsonRes.message;
+	})
+}
